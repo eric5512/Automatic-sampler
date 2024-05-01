d5e7483408fc854720342ed8ce8cdad59ff90416
Eric <ericdandria2@gmail.com>
2024-05-01 02:44:43 +0200

    Firmware update, ready to test

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,5 +1,7 @@
 #include <MovementHelper.h>
 
+#define BAUD_RATE 9600
+
 extern "C" {
   void setup();
   void loop();
@@ -8,17 +10,47 @@ extern "C" {
 MovementHelper *mh = MovementHelper::get_instance();
 
 void setup() {
+  Serial.begin(BAUD_RATE); // Init serial communication
+
   mh->origin(); // Get all the motors to 0 position
-
-  // if (mov.automatic)
-  //   automatic_movement();
-  // else
-  //   manual_movement();
-
-  // delay(2000);
-
-  mh->move(MOT_X, CW, 399);
-  mh->move(MOT_Y, CW, 10);
 }
 
-void loop() {}
+void loop() {
+  if (Serial.available() > 2) {
+    String msg = Serial.readString();
+    Motor mot;
+    bool res;
+
+    switch (msg[0]) {
+      case 'X':
+        mot = MOT_X;
+        break;
+      
+      case 'Y':
+        mot = MOT_Y;
+        break;
+      
+      case 'Z':
+        mot = MOT_Z;
+        break;
+      
+      default:
+        mot = MOT_X;
+        break;
+    }
+
+    coord_t dist;
+    if (msg[1] == '+') { // If number begins with + or - the movement is relative, if not, it's absolute
+      dist = msg.substring(2).toInt();
+      res = mh->move(mot, CW, dist);
+    } else if (msg[1] == '-') {
+      dist = msg.substring(2).toInt();
+      res = mh->move(mot, CCW, dist);
+    } else {
+      dist = msg.substring(1).toInt();
+      res = mh->move(mot, dist);
+    }
+
+    Serial.print(res ? 1 : 0);
+  }
+}
